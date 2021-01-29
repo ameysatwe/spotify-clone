dfdf883c0ddeadac5f0874df7a119a9dd5192edb
Amey Satwe <amey10051999@gmail.com>
2021-01-29 20:45:42 +0530

    initial-Login page done

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Login.css";
+function Login() {
+	return (
+		<div className="login">
+			<img
+				src="https://i.pinimg.com/originals/27/6f/27/276f273d11f8b9dbc0a9c55bb38ea8c6.gif"
+				alt="Logo"
+			></img>
+			<a href="/">Login To Spotify</a>
+		</div>
+	);
+}
+//https://i.pinimg.com/originals/27/6f/27/276f273d11f8b9dbc0a9c55bb38ea8c6.gif
+export default Login;
