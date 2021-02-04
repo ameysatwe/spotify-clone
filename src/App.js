197cb99c0eb67916b218ce41fe8ac52e1186e1e3
Amey Satwe <amey10051999@gmail.com>
2021-02-05 00:36:20 +0530

    reducer and context API usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromResponse } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 const spotify = new SpotifyWebApi();
 function App() {
-	const [token, setToken] = useState(null);
+	const [{ user, token }, dispatch] = useDataLayerValue();
 	useEffect(() => {
 		const hash = getTokenFromResponse();
 		window.location.hash = "";
 		const _token = hash.access_token;
 		if (_token) {
-			setToken(_token);
+			dispatch({
+				type: "SET_TOKEN",
+				token: _token,
+			});
 			spotify.setAccessToken(_token);
 			spotify.getMe().then((user) => {
-				console.log(user);
+				dispatch({
+					type: "SET_USER",
+					user: user,
+				});
 			});
 		}
-	}, []);
+	}, [user, dispatch]);
+	console.log(token);
 	return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+	user: null,
+	playlists: [],
+	playing: false,
+	item: null,
+	// Make it null after dev
+	token:
+		"BQCyW4UE20w1Knd0Jt1rUr_8YcRh0xZak_UFdGOxAhFHrT_3mA…zgchVm_7S_xUQYQM5a_bfWk8nKN1Bgro3y31iLJqEv2JjYEZq",
+};
+
+const reducer = (state, action) => {
+	console.log(action);
+
+	switch (action.type) {
+		case "SET_USER":
+			return {
+				...state,
+				user: action.user,
+			};
+		case "SET_TOKEN":
+			return {
+				...state,
+				token: action.token,
+			};
+		default:
+			return state;
+	}
+};
+
+export default reducer;
