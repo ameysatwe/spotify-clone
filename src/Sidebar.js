aa78e4c47310e9341155d822ca15bb220707c838
Amey Satwe <amey10051999@gmail.com>
2021-02-05 17:38:51 +0530

    update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Sidebar.css";
 function Sidebar() {
-	return <div className="sidebar">s</div>;
+	return (
+		<div className="sidebar">
+			<img
+				className="sidebar__logo"
+				src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+				alt="logo"
+			/>
+		</div>
+	);
 }
 
 export default Sidebar;
